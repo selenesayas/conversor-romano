153b47f25233b68f749c95e62078dabca4e01acb
selenesayas <selenesayas0604@gmail.com>
2025-11-25 23:05:16 -0300

    Arreglada validación romana a árabe y agregado CORS

--- api/r2a.js
+++ api/r2a.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-
+  // Habilitar CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -12,13 +12,23 @@ export default function handler(req, res) {
 
   const r = roman.toUpperCase();
 
-  // Validación estricta de números romanos clásicos
-  const romanoValido = /^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
-  if (!romanoValido.test(r)) {
-    return res.status(400).json({ error: "Número romano inválido." });
+  // Validación de sustracción y orden incorrecto
+  const invalidPatterns = [
+    /IL|IC|ID|IM/,      // I no puede restarse de L,C,D,M
+    /VX|VL|VC|VD|VM/,   // V no puede restarse
+    /XD|XM/,             // X no puede restarse de D o M
+    /LC|LD|LM/,          // L no puede restarse
+    /DM/,                // D no puede restarse
+    /(I{4,}|X{4,}|C{4,}|M{4,}|V{2,}|L{2,}|D{2,})/ // máximos repetidos
+  ];
+
+  for (const pat of invalidPatterns) {
+    if (pat.test(r)) {
+      return res.status(400).json({ error: "Número romano inválido" });
+    }
   }
 
-  const mapa = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  const mapa = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
   let total = 0;
 
   for (let i = 0; i < r.length; i++) {
@@ -34,4 +44,4 @@ export default function handler(req, res) {
   }
 
   return res.status(200).json({ arabic: total });
-}}
+}
