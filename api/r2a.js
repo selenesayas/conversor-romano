d795f17bbaa9e1995f9c9179fb7f9a0efb222557
selenesayas <selenesayas0604@gmail.com>
2025-11-25 23:07:43 -0300

    Arreglar validación de estructura romana en /r2a para devolver 400

--- api/r2a.js
+++ api/r2a.js
@@ -1,9 +1,4 @@
 export default function handler(req, res) {
-  // Habilitar CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
   const { roman } = req.query;
 
   if (!roman || !/^[IVXLCDMivxlcdm]+$/.test(roman)) {
@@ -12,23 +7,13 @@ export default function handler(req, res) {
 
   const r = roman.toUpperCase();
 
-  // Validación de sustracción y orden incorrecto
-  const invalidPatterns = [
-    /IL|IC|ID|IM/,      // I no puede restarse de L,C,D,M
-    /VX|VL|VC|VD|VM/,   // V no puede restarse
-    /XD|XM/,             // X no puede restarse de D o M
-    /LC|LD|LM/,          // L no puede restarse
-    /DM/,                // D no puede restarse
-    /(I{4,}|X{4,}|C{4,}|M{4,}|V{2,}|L{2,}|D{2,})/ // máximos repetidos
-  ];
-
-  for (const pat of invalidPatterns) {
-    if (pat.test(r)) {
-      return res.status(400).json({ error: "Número romano inválido" });
-    }
+  // Validación de estructura romana correcta (repeticiones y sustracciones)
+  const regexValido = /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+  if (!regexValido.test(r)) {
+    return res.status(400).json({ error: "Formato inválido: estructura romana incorrecta." });
   }
 
-  const mapa = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+  const mapa = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
   let total = 0;
 
   for (let i = 0; i < r.length; i++) {
